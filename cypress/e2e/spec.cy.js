47571790758b6975ac76f9f94775ef54e31752ef
Jacek Drobiecki <jacek.drobiecki@gmail.com>
2023-01-31 12:25:19 +0100

    fixed example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,33 +1,48 @@
-import chai from 'chai'
-
 const customAssertions = (_chai, utils) => {
-  function someCustomMethod(subject) {
+  function someCustomMethod() {
     new _chai.Assertion(this._obj).to.exist;
-    utils.flag("insideSomeCustomChain", true);
+    utils.flag(this, "insideSomeCustomChain", true);
   }
+
+  function orginalTargetBlank() {
+    assert.fail("it should never be called");
+  }
+
   function _someCustomMethodExtension(_super) {
-    function someCustomMethodExtension(subject) {
+    return function someCustomMethodExtension() {
       if (utils.flag(this, "insideSomeCustomChain")) {
-        new _chai.Assertion(this._obj).to.have.property("data-testid");
+        new _chai.Assertion(this._obj).to.have.property("target", "_blank");
       } else {
         _super.apply(this, arguments);
       }
     }
   }
-  _chai.Assertion.addChainableMethod("someCustomMethod", someCustomMethod);
+  _chai.Assertion.addChainableMethod("customExist", someCustomMethod);
+  _chai.Assertion.addMethod("targetBlank", orginalTargetBlank);
   _chai.Assertion.overwriteMethod(
-    "overwrittenMethod",
+    "targetBlank",
     _someCustomMethodExtension
   );
 };
 
-chai.use(customAssertions);
+if (!customAssertions.applied) {
+    chai.use(customAssertions);
+    customAssertions.applied = true;
+}
 
 describe("template spec", () => {
-  it("passes", () => {
+  it("currently doesn't work", () => {
     cy.visit("https://example.cypress.io");
-    cy.get('[href="/commands/querying"]')
-      .should("someCustomMethod")
-      .and("overwrittenMethod");
+    cy.get('[href="https://www.cypress.io"]')
+      .should("to.customExist")
+      .and("have.targetBlank");
+  });
+
+  it("should work like this", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get('[href="https://www.cypress.io"]')
+      .should(($el) => {
+        expect($el).to.customExist().and.have.targetBlank();
+      })
   });
 });
